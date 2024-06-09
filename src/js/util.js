6be13d0c8fcbb1681aa7cfaa8be36cfafa5ae115
FerozDevv <148396632+FerozDevv@users.noreply.github.com>
2024-06-09 15:33:17 -0300

    Add files via upload

---
+++ src/js/util.js
@@ -0,0 +1,5 @@
+export const weatherForecastMap = new Map();
+
+weatherForecastMap.set("Clouds", "Céu com Nuvens")
+weatherForecastMap.set("Clear", "Céu limpo")
+weatherForecastMap.set("Rain", "Chuva")
