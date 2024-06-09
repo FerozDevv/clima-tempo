9a02502a8c16291ef5ab7469934d436e70142c1d
FerozDevv <148396632+FerozDevv@users.noreply.github.com>
2024-06-09 15:33:56 -0300

    Delete util.js

--- util.js
+++
@@ -1,5 +0,0 @@
-export const weatherForecastMap = new Map();
-
-weatherForecastMap.set("Clouds", "Céu com Nuvens")
-weatherForecastMap.set("Clear", "Céu limpo")
-weatherForecastMap.set("Rain", "Chuva")
